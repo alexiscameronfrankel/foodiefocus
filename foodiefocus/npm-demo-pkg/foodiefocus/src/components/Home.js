3c8b34bf9b9f9e1c5dae30140fedb4a58e9dbb44
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-02-27 11:38:49 -0500

    working on styling homepage

--- foodiefocus/npm-demo-pkg/foodiefocus/src/components/Home.js
+++ foodiefocus/npm-demo-pkg/foodiefocus/src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import {
-    Link
-  } from "react-router-dom";
+import {Link} from "react-router-dom";
+import Card from 'react-bootstrap/Card';
 
 
 class Home extends Component {
@@ -41,19 +40,26 @@ class Home extends Component {
     render() {
         return (
             <div>
-                 <Link to="/maintimer">
-                    <p>Click here to start working</p>
-
-
-                </Link>
-
-
-
-                <form onSubmit={this.submitting}>
-                <label>Enter your podcast genre (ex:meditation, nutrition, dogs)</label><br/>
-                <input type="text" id="fname" name="name" onChange={this.handlePersonTyping}/><br/>
-                <input type="submit" value="Submit"/>
+                <div className="row d-flex justify-content-center">
+                <Card className="home_container col-sm-6 " border="danger" style={{ width: '50%'}}>
+                    <Card.Img variant="top" src="./tomatoman.png" style={{ width: '30%'}}/>
+                    <Card.Body>
+                        Enter your podcast genre (ex:meditation, nutrition, dogs)
+                        Halvah croissant biscuit fruitcake topping. Croissant apple pie pie. Cookie jelly bear claw powder candy canes marshmallow soufflé cake. Powder sugar plum croissant candy croissant gummi bears. Donut jelly beans ice cream liquorice ice cream cake. Marzipan cake icing chupa chups dragée jujubes tart. Bonbon cake chupa chups wafer chupa chups marshmallow sesame snaps.
+                        <form onSubmit={this.submitting} className="home-main">
+                    {/* <label className="home-main_text">
+                    </label> */}
+                    {/* <div> */}
+                        <input type="text" id="fname" name="name" onChange={this.handlePersonTyping} className="home-main_input"/>
+                        
+                        <Link to="/maintimer"><input type="submit" value="Submit" className="home-main_submit"/> </Link>
+                        
+                    {/* </div> */}
                 </form>
+                    </Card.Body>
+                </Card>
+              
+                </div>
             </div>
         );
     }
